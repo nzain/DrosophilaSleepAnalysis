2c8770ed234501640d275e53b9cd1bee9f28717e
Patrick Stalph <patrick.stalph@gmx.de>
2019-04-21 13:36:13 +0200

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -5,19 +5,26 @@ SET prj=%prjDir%\DroSleep.App.csproj
 SET ini=%prjDir%\DroSleep.ini
 SET deployDir=%~dp0Release
 SET deployWin64=%deployDir%\win-x64
-SET deployExe=%deployDir%\DroSleep.exe
+SET deployLinux64=%deployDir%\linux-x64
 
 ECHO publish to: %deployDir%
 
 IF EXIST "%deployDir%" rmdir /s /q "%deployDir%"
 
 dotnet publish -c Release -r win-x64 -o "%deployWin64%" "%prj%"
+dotnet publish -c Release -r linux-x64 -o "%deployLinux64%" "%prj%"
 xcopy /d /s "%ini%" "%deployDir%"
 
 .\windows-x64.warp-packer.exe^
 	--arch windows-x64^
 	--input_dir "%deployWin64%"^
 	--exec DroSleep.exe^
-	--output "%deployExe%"
+	--output "%deployDir%\DroSleep.exe"
+	
+.\windows-x64.warp-packer.exe^
+	--arch linux-x64^
+	--input_dir "%deployLinux64%"^
+	--exec DroSleep^
+	--output "%deployDir%\DroSleep_linux"
 
 PAUSE
